3d9275da78e87f50904d0863612bee2528629723
Lembaad-imad <lembaad.imad.lm@gmail.com>
2026-07-07 10:59:36 +0100

    fix the cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors";
 import openapiGlue from "fastify-openapi-glue";
 import productController from "./controllers/product.controller.js";
 import authController from './controllers/auth.controller.js';
@@ -16,20 +17,25 @@ function buildApp(opts = {}) {
     ...opts,
   });
 
+  fastify.register(cors, {
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   fastify.register(jwtPlugin);
 
   const serviceHandlers = {
     ...productController,
     ...authController,
   };
-fastify.use({cros:({origin:'*'})})
 
   fastify.register(openapiGlue, {
     specification: "./openapi.yaml",
     serviceHandlers: serviceHandlers,
-    securityHandlers:{
-      bearerAuth: async (req,reply ,params) =>{
-        await req.server.authenticate(req,reply)
+    securityHandlers: {
+      bearerAuth: async (req, reply, params) => {
+        await req.server.authenticate(req, reply)
       }
     },
     prefix: "/api",
